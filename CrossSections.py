95b874ea57789058655d8704e381e606f64907fe
nigomezcr <nigomezcr@unal.edu.co>
2023-11-22 10:47:38 -0500

    Added Fits with PPX-Sec, working with Correa Data

--- CrossSections.py
+++ CrossSections.py
@@ -121,134 +121,7 @@ def Transfer_sigma_attractive(v, g=gp, M=mZp ,m=mDM ):
 
 
 def Transfer_sigma(v, g=gp, M=mZp ,m=mDM ):
-    return (Transfer_sigma_repulsive(v, g, M, m) + Transfer_sigma_attractive(v, g, M, m))/2
-
-"""
-///////////// Scattering Cross Sections Computed Numerically  ///////////////
-"""
-
-#### Total cross section, numerical #####
-# t channel
-def sigma_t_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(2*d*np.pi*s**2*v**2*m))
-    integrand = lambda t: (t**2 + 2*s*t + 2*s**2*v**2 + 8*m**4)/(t-M**2)**2
-    return fc*Prefactor*quad(integrand, -s*v**2, 0)[0]
-
-# u channel
-def sigma_u_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(2*d*np.pi*s**2*v**2*m))
-    integrand = lambda t: (t**2 - 8*m**2*(s + t) + s**2 + 24*m**4)/(t + s*v**2 + M**2)**2
-    return fc*Prefactor*quad(integrand, -s*v**2, 0)[0]
-
-# t-u interference
-def sigma_tu_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(2*d*np.pi*s**2*v**2*m))
-    integrand = lambda t: (4*m**4 - 2*(v**2 + 3)*m**2*s + s**2)/((t-M**2)*(t + s*v**2 + M**2))
-    return - fc *Prefactor * quad(integrand, -s*v**2, 0)[0]
-
-
-# s-channel
-def sigma_s_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(16*d*np.pi*s**2*v**2*m))
-    integrand = lambda t:  (8*((2*m**2 + s*v**2 + t)**2 - s*(s-2*m**2) + (2*m**2 - t)**2) - 2*s*(4*(s-2*m**2) - 8*s))/(s - M**2)**2
-    return fc *Prefactor * quad(integrand, -s*v**2, 0)[0]
-
-# s-t interference
-def sigma_st_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(16*d*np.pi*s**2*v**2*m))
-    integrand = lambda t:  - 8*(4*m**4 + 6*m**2*s*v**2 + 2*m**2*s + 8*m**2*t + s**2*v**4 + 2*s*v**2*t + t**2)/((s-M**2)*(t-M**2))
-    return fc *Prefactor * quad(integrand, -s*v**2, 0)[0]
-
-
-# Attractive
-def sigma_a_Num(v, g, M, m):
-    return sigma_t_num(v, g, M, m) + sigma_s_num(v, g, M, m) + sigma_st_num(v, g, M, m)
-
-# Repulsive
-def sigma_r_Num(v, g, M, m):
-    return sigma_t_num(v, g, M, m) + sigma_u_num(v, g, M, m) + 2*sigma_tu_num(v, g, M, m)
-
-
-# Total effective
-def sigma_eff_Num(v, g, M, m):
-    return (sigma_a_Num(v, g, M, m))# + sigma_r_Num(v, g, M, m) )/2
-
-#### Transfer cross section, numerical #####
-# t channel
-def sigmaT_t_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(2*d*np.pi*s**2*v**2*m))
-    integrand = lambda t: t*(t**2 + 2*s*t + 2*s**2*v**2 + 8*m**4)/(t-M**2)**2
-    return -2/(s*v**2) * fc*Prefactor*quad(integrand, -s*v**2, 0)[0]
-
-# u channel
-def sigmaT_u_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(2*d*np.pi*s**2*v**2*m))
-    integrand = lambda t: t*(t**2 - 8*m**2*(s + t) + s**2 + 24*m**4)/(t + s*v**2 + M**2)**2
-    return -2/(s*v**2) * fc*Prefactor*quad(integrand, -s*v**2, 0, )[0]
-
-# t-u interference
-def sigmaT_tu_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = -(g**4/(2*d*np.pi*s**2*v**2*m))
-    integrand = lambda t: t*(4*m**4 - 2*(v**2 + 3)*m**2*s + s**2)/((t-M**2)*(t + s*v**2 + M**2))
-    return  -2/(s*v**2) * fc *Prefactor * quad(integrand, -s*v**2, 0)[0]
-
-
-# s-channel
-def sigmaT_s_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(16*d*np.pi*s**2*v**2*m))
-    integrand = lambda t:  t*(8*((2*m**2 + s*v**2 + t)**2 - s*(s-2*m**2) + (2*m**2 - t)**2) - 2*s*(4*(s-2*m**2) - 8*s))/(s - M**2)**2
-    return -2/(s*v**2) * fc *Prefactor * quad(integrand, -s*v**2, 0)[0]
-
-# s-t interference
-def sigmaT_st_num(v, g, M, m):
-    d = 4 #Dirac
-    v = v/2
-    s = 4*m**2/(1 - v**2)
-    Prefactor = (g**4/(16*d*np.pi*s**2*v**2*m))
-    integrand = lambda t:  - t*8*(4*m**4 + 6*m**2*s*v**2 + 2*m**2*s + 8*m**2*t + s**2*v**4 + 2*s*v**2*t + t**2)/((s-M**2)*(t-M**2))
-    return -2/(s*v**2) * fc *Prefactor * quad(integrand, -s*v**2, 0)[0]
-
-
-# Attractive Transfer
-def sigmaT_a_Num(v, g, M, m):
-    return sigmaT_t_num(v, g, M, m) + sigmaT_s_num(v, g, M, m) + sigmaT_st_num(v, g, M, m)
-
-# Repulsive Transfer
-def sigmaT_r_Num(v, g, M, m):
-    return sigmaT_t_num(v, g, M, m) + sigmaT_u_num(v, g, M, m) + 2*sigmaT_tu_num(v, g, M, m)
-
-
-# Transfer effective
-def sigmaT_Num(v, g, M, m):
-    return (sigmaT_a_Num(v, g, M, m) + sigmaT_r_Num(v, g, M, m) )/2
-
+    return (Transfer_sigma_repulsive(v/c, g, M, m) + Transfer_sigma_attractive(v/c, g, M, m))/2
 
 
 """
@@ -270,12 +143,25 @@ def sigv_T(v0, g, M, m):
     Integral = quad(sigv_integrand, 0.1, vmax, args=(v0, g, M, m))[0]
     return Prefactor*Integral
 
-# Ensemble average cross section for Transfer in low energy
+
+def Transfer_SigV_integrand(v, v0, g, M, m):
+    return total_sigma(v, g, M, m)*v*np.exp(-0.5*v**2/v0**2)*v**2
+
+
+def Transfer_SigmaV(v0, g=gp, M=mZp, m=mDM):
+    sigma2_MB = v0**2*np.pi*(3*np.pi - 8)/np.pi
+    vmax = 2*np.sqrt(sigma2_MB)
+
+    Prefactor = 4*np.pi/((2*np.pi*v0**2)**1.5 * m)
+    Integral = quad(Transfer_SigV_integrand, 0.1, vmax, args=(v0, g, M, m))[0]
+    return Prefactor*Integral
+
+
 def sigv_integrand_low(v, v0, g, M, m):
     return Transfer_Sigma_Low_Energy(v, g, M, m)*v*np.exp(-0.5*v**2/v0**2)*v**2
 
 
-def Transfer_Sigmav_Low_Energy(v0, g, M, m):
+def Transfer_Sigmav_Low_Energy(v0, g=gp, M=mZp, m=mDM):
     sigma2_MB = v0**2*np.pi*(3*np.pi - 8)/np.pi
     vmax = 2*np.sqrt(sigma2_MB)
 
