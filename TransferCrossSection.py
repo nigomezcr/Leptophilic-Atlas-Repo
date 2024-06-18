d1e7d2a11a2f63376dca576dada145263f0ab16d
nigomezcr <nigomezcr@unal.edu.co>
2024-06-18 15:06:56 -0500

    Updated TransferCrossSection plot, new colors and models

--- TransferCrossSection.py
+++ TransferCrossSection.py
@@ -1,17 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from PlotSettings import MainColor1, MainColor2, MainColor3, BackgroundColor1, BackgroundColor2
-from CrossSections import Transfer_sigma
+from PlotSettings import MainColor1, MainColor2, MainColor3, BackgroundColor1, BackgroundColor2, BackgroundColor3, Gray1
 
 # Arrays
 mDM_array = np.logspace(-1, 2, 100)
-
 v0 = (10, 1000)
-M = (2, 20)
-gp = ( 8e-3, 4e-2 )
 
-line_colors = (MainColor2, MainColor1)
-line_styles = ('-', 'dashed')
+alphaDM, alphaDM2, alphaDM3 = 0.005, 0.01, 0.03
+mZp, mZp2, mZp3 = 9.8, 20, 15
+mDM, mDM2, mDM3 = 6.9, 165, 8
+
+
+def sigmatransfer(V, mphi, mchi, alphax):
+    w = 300 * (mphi / 10) * (10 / mchi)
+    st = (275.73) * (alphax / 1e-2) ** 2 * (mchi / 10) * (10 / mphi) ** (4)
+    sv = 2 * st * (w ** 4 / V ** 4) * (2 * np.log(1.0 + V ** 2 / (2 * w ** 2)) - np.log(1.0 + V ** 2 / (w ** 2)))
+    return sv
+
+###### For the Plots ######
+
+line_colors = (MainColor1, MainColor3)
+line_styles = ('solid', 'dashed', 'dotted')
 
 def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
     c1=np.array(c1)
@@ -21,72 +30,50 @@ def colorFader(c1,c2,mix=0): #fade (linear interpolate) from color c1 (at mix=0)
 n = 500
 c1 = BackgroundColor2  # yellow
 c2 = (1, 1, 1)  # white
-y_values = np.logspace(-3, 0, n+1)  # Generate logarithmically spaced values
+y_values = np.logspace(-1.6, 0, n+1)  # Generate logarithmically spaced values
 
 
 #Plots
-fig, ax = plt.subplots(1, 2, figsize=(10,6))
+fig, ax = plt.subplots(1, 1)
+
+ax.text(mDM_array[5], 0.2, 'Clusters', fontsize=14)
+for y in y_values:
+    ax.axhline(y, 0.0, 1, color=colorFader(c2, c1, (np.log10(y) + 3) / 4), linewidth=4)
+
+ax.text(mDM_array[5], 20, 'Dwarfs')
+ax.fill_between([mDM_array[0], mDM_array[-1]], 10, 100, color=BackgroundColor1, alpha=0.7)
+
+ax.set_ylabel(r'$\sigma_T /m_{\chi} ~[ \mathrm{cm^2/g}]$')
+ax.set_xlabel(r'$m_{{\chi}} ~[\mathrm{GeV} ] $')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_xlim([mDM_array[0], mDM_array[-1]])
+ax.set_ylim(1e-2, 200)
+
+Dwarf1 = [sigmatransfer(v0[0], mZp, m, alphaDM) for m in mDM_array]
+Dwarf2 = [sigmatransfer(v0[0], mZp2, m, alphaDM2) for m in mDM_array]
+Dwarf3 = [sigmatransfer(v0[0], mZp3, m, alphaDM3) for m in mDM_array]
+
+Cluster1 = [sigmatransfer(v0[1], mZp, m, alphaDM) for m in mDM_array]
+Cluster2 = [sigmatransfer(v0[1], mZp2, m, alphaDM2) for m in mDM_array]
+Cluster3 = [sigmatransfer(v0[1], mZp3, m, alphaDM3) for m in mDM_array]
 
 
-for i in range(2):
+DwPlot1 = ax.plot(mDM_array, Dwarf1, linestyle='solid', color=MainColor1, label='DM1')
+DwPlot2 = ax.plot(mDM_array, Dwarf2, linestyle='dashed', color=MainColor1, label='DM2')
+DwPlot3 = ax.plot(mDM_array, Dwarf3, linestyle='dotted', color=MainColor1, label='DM3')
 
-    #ax[i].text(2e-2, 1e-2, r'$ g^{{\prime}} = {}$'.format(gp[i]))
-    ax[i].set_title(r'$ g^{{\prime}} = {}$'.format(gp[i]))
-        
+ClPlot1 = ax.plot(mDM_array, Cluster1, linestyle='solid',  color=MainColor2, label='DM1')
+ClPlot2 = ax.plot(mDM_array, Cluster2, linestyle='dashed', color=MainColor2, label='DM2')
+ClPlot3 = ax.plot(mDM_array, Cluster3, linestyle='dotted', color=MainColor2, label='DM3')
 
-    
-    ax[i].text(mDM_array[5], 0.2, 'Clusters')
-    for y in y_values:
-        ax[i].axhline(y, 0.0, 1, color=colorFader(c2, c1, (np.log10(y) + 3) / 4), linewidth=4)
-    
-    ax[i].text(mDM_array[5], 20, 'Dwarfs')
-    ax[i].fill_between([mDM_array[0], mDM_array[-1]], 10, 50, color=MainColor2, alpha=0.7)
+first_legend = ax.legend(handles=[DwPlot1[0], DwPlot2[0], DwPlot3[0]], loc=4, title='Models')
+ax.add_artist(first_legend)
 
-    
-    for j in range(2):
-        sm_T1 = [Transfer_sigma(v0[j], gp[i], M[0], m) for m in mDM_array]
-        sm_T2 = [Transfer_sigma(v0[j], gp[i], M[1], m) for m in mDM_array]
-        
-    
-        ax[i].plot(mDM_array, sm_T1, linestyle=line_styles[0], color=line_colors[j], label=r'$m_{{Z\prime}}= {} \mathrm{{MeV}}$'.format(M[0]*1e3) )
-        ax[i].plot(mDM_array, sm_T2, linestyle=line_styles[1], color=line_colors[j],  label=r'$m_{{Z\prime}}= {} \mathrm{{MeV}}$'.format(M[1]*1e3) )
-        
+ax.legend(handles=[DwPlot1[0], ClPlot1[0]], loc=2, title='Velocity')
 
-
-
-
-    ax[i].set_ylabel(r'$\sigma_T /m_{\chi} ~[ \mathrm{cm^2/g}]$')
-    ax[i].set_xlabel(r'$m_{{\chi}} ~[\mathrm{GeV} ] $')
-    ax[i].set_xscale('log')
-    ax[i].set_yscale('log')
-    ax[i].set_xlim([mDM_array[0], mDM_array[-1]])
-    ax[i].set_ylim(1e-7, 200)
-    #ax[i].legend()
-
-
-# Mass legends
-Leg_x = 0.2
-hline_x1 = 0.02
-hline_x2 = 0.08
-leg_m1 = 1e-4 #mass 1
-leg_m2 = 1e-5 #mass 2
-leg_v1 = 1e-5 #vel 1
-leg_v2 = 1e-6 #vel 2
-                
-
-ax[0].axhline(1.2*leg_m1, hline_x1, hline_x2,  color='k')
-ax[0].axhline(1.2*leg_m2, hline_x1, hline_x2,  color='k', linestyle='dashed')
-ax[0].text(Leg_x, leg_m1, r'$m_{Z^{\prime}} = 2~\mathrm{MeV}$')
-ax[0].text(Leg_x, leg_m2, r'$m_{Z^{\prime}} = 50~\mathrm{MeV}$')
-
-# Velocity legends
-ax[1].axhline(1.2*leg_v1, hline_x1, hline_x2, color=MainColor2)
-ax[1].axhline(1.2*leg_v2, hline_x1, hline_x2, color=MainColor1)
-ax[1].text(Leg_x, leg_v1, r'$v = 10~\mathrm{km/s}$')
-ax[1].text(Leg_x, leg_v2, r'$v =  1000~\mathrm{km/s}$')
 
 
 
 fig.tight_layout()
-fig.show()
 fig.savefig('Plots/TransferCrossSections.pdf')
