DarkMatterFits corrected -> extra factor 1/m in <sT/m>

PlotTransferSigma.py
@@ -2,25 +2,38 @@ import  numpy as np
 from Constants import c, fc
 import matplotlib.pyplot as plt
 
-def sigmaviscosity(V, mphi=19.9, mchi=161.5, alphax=0.01):
+def sigmaviscosity(V, mphi=19.9, mchi=16, alphax=0.01):
     w  = 300*(mphi/(10))*(10/(mchi))
     st = (275.73)*(alphax/0.01)**2*(mchi/10.0)*(10.0/(mphi))**(4)
     sv = 3*st*(w**6/(V**6))*( (2+V**2/(w**2))*np.log(1+V**2/(w**2))-2*V**2/w**2)
     return sv
 
-def sigmatransfer(V, mphi=19.9, mchi=161.5, alphax=0.01):
+def sigmatransfer(V, mphi=19.9, mchi=160, alphax=0.01):
     w = 300 * (mphi / 10) * (10 / mchi)
     st = (275.73) * (alphax / 1e-2) ** 2 * (mchi / 10) * (10 / mphi) ** (4)
     sv = 2 * st * (w ** 4 / V ** 4) * (2 * np.log(1.0 + V ** 2 / (2 * w ** 2)) - np.log(1.0 + V ** 2 / (w ** 2)))
     return sv
 
+def sigmaviscosity_alter(V, mphi=19.9, mchi=16, alphax=0.01):
+    w  = 300*(mphi/(10))*(10/(mchi))
+    beta = V/w
+    st = (275.73)*(alphax/0.01)**2*(mchi/10.0)*(10.0/(mphi))**(4)
 
-v_array = np.logspace(1, 3, 100)
-crossSection_array_transfer = [sigmatransfer(v) for v in v_array]
+    if beta < 0.01:
+        sv = 3/6*st
+    else:
+        sv = 3*st*(1/beta**6)*( (2+beta**2)*np.log(1+ beta**2)- 2*beta**2 )
+    return sv
+
+
+
+v_array = np.logspace(0.2, 1, 100)
+crossSection_array_transfer = [sigmaviscosity_alter(v) for v in v_array]
 crossSection_array_viscosity = [sigmaviscosity(v) for v in v_array]
 
 plt.plot(v_array, crossSection_array_transfer, 'k')
 plt.plot(v_array, crossSection_array_viscosity, 'g')
+
 
 
 plt.xscale('log')
@@ -28,4 +41,4 @@ plt.yscale('log')
 plt.ylabel('Cross Section')
 plt.xlabel('velocity')
 plt.savefig('Plots/CrossSectionsTest.pdf')
-plt.show()
+#plt.show()

TransferCrossSection.py
@@ -6,9 +6,9 @@ from PlotSettings import MainColor1, MainColor2, MainColor3, BackgroundColor1, B
 mDM_array = np.logspace(0, 2, 100)
 v0 = (10, 1000)
 
-alphaDM, alphaDM2, alphaDM3 = 0.01, 0.01, 0.03
-mZp, mZp2, mZp3 = 19.9, 25.1, 15
-mDM, mDM2, mDM3 = 161.5, 209.6, 8
+alphaDM, alphaDM2, alphaDM3 = 0.01, 0.01, 0.01
+mZp, mZp2, mZp3 = 19.9, 25.1, 50
+mDM, mDM2, mDM3 = 161.5, 209.6, 7
 
 ######## Functions ############
 def sigmatransfer(V, mphi, mchi, alphax):
@@ -19,9 +19,15 @@ def sigmatransfer(V, mphi, mchi, alphax):
 
 def sigmaviscosity(V, mphi, mchi, alphax):
     w  = 300*(mphi/(10))*(10/(mchi))
+    beta = V/w
     st = (275.73)*(alphax/0.01)**2*(mchi/10.0)*(10.0/(mphi))**(4)
-    sv = 3*st*(w**6/(V**6))*( (2+V**2/(w**2))*np.log(1+V**2/(w**2))-2*V**2/w**2)
+
+    if beta < 0.01:
+        sv = 3/6*st
+    else:
+        sv = 3*st*(1/beta**6)*( (2+beta**2)*np.log(1+ beta**2)- 2*beta**2 )
     return sv
+
 
 ###### For the Plots ######
 line_colors = (MainColor1, MainColor3)
@@ -48,7 +54,7 @@ for y in y_values:
 ax.text(mDM_array[5], 20, 'Dwarfs')
 ax.fill_between([mDM_array[0], mDM_array[-1]], 10, 100, color=BackgroundColor1, alpha=0.7)
 
-ax.set_ylabel(r'$\sigma_T /m_{\chi} ~[ \mathrm{cm^2/g}]$')
+ax.set_ylabel(r'$\sigma /m_{\chi} ~[ \mathrm{cm^2/g}]$')
 ax.set_xlabel(r'$m_{{\chi}} ~[\mathrm{GeV} ] $')
 ax.set_xscale('log')
 ax.set_yscale('log')
@@ -57,15 +63,18 @@ ax.set_ylim(1e-2, 200)
 
 Dwarfv = [sigmaviscosity(v0[0], mZp, m, alphaDM) for m in mDM_array]
 Dwarft = [sigmatransfer(v0[0], mZp2, m, alphaDM2) for m in mDM_array]
+Dwarf_chi2 = [sigmatransfer(v0[0], mZp3, m, alphaDM3) for m in mDM_array]
 
 
 Clusterv = [sigmaviscosity(v0[1], mZp, m, alphaDM) for m in mDM_array]
 Clustert = [sigmatransfer(v0[1], mZp2, m, alphaDM2) for m in mDM_array]
+Cluster_chi2 = [sigmatransfer(v0[1], mZp3, m, alphaDM3) for m in mDM_array]
 
 
 
 DwPlotv = ax.plot(mDM_array, Dwarfv, linestyle='solid', color=MainColor1)
 DwPlott = ax.plot(mDM_array, Dwarft, linestyle='dashed', color=MainColor1)
+#DwPlot_chi2 = ax.plot(mDM_array, Dwarf_chi2, linestyle='dashed', color=BackgroundColor3)
 
 ClPlotv = ax.plot(mDM_array, Clusterv, linestyle='solid',  color=MainColor2)
 ClPlott = ax.plot(mDM_array, Clustert, linestyle='dashed', color=MainColor2)